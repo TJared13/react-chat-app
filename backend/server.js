93c68a2a2a2b886f96dfeb450b30fb0cb31d1e99
TJared13 <trevin.jared@gmail.com>
2025-02-12 21:36:19 -0700

    save

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+const app = express();
+
+dotenv.config();
+const PORT=process.env.SERVER_PORT || 5000;
+
+app.get("/", (req, res) => {
+    //root route http://localhost:3131/
+    res.send("Hello World!!");
+});
+
+
+
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
